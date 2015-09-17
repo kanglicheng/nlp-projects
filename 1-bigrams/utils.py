ef9f13e538ff6278f3abecd9c34f8a0d618a010a
fredcallaway <fredc@llaway.com>
2015-09-16 22:25:28 -0400

    get_text_files() and main()

--- 1-bigrams/utils.py
+++ 1-bigrams/utils.py
@@ -1,3 +1,4 @@
+import os
 
 class lazy_property(object):
     """Meant to be used for lazy evaluation of an object attribute.
@@ -16,3 +17,9 @@ class lazy_property(object):
         value = self.fget(obj)
         setattr(obj,self.func_name,value)
         return value
+
+
+def get_text_files(directory):
+    nested_files = [w[2] for w in os.walk(directory)]
+    files = sum(nested_files, [])
+    return [f for f in files if f.endswith('.txt')]
